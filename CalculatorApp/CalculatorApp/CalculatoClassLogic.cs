2c4a89950c3e44cf3a864a1855cc68b6d3d2fd51
Aoigan2k4 <127539958+Aoigan2k4@users.noreply.github.com>
2025-02-20 12:31:16 -0500

    Initial Commit

---
+++ CalculatorApp/CalculatorApp/CalculatoClassLogic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+   static class CalculatoClassLogic
+   {
+        public static double calculate(double value1, double value2, string mathOperator)
+        {
+            double result = 0;
+
+            switch (mathOperator)
+            {
+                case "/":
+                    if (value2 != 0)
+                    {
+                        result = value1 / value2;
+                    }
+                 
+                    break;
+                case "*":
+                    result = value1 * value2;
+                    break;
+                case "+":
+                    result = value1 + value2;
+                    break;
+                case "-":
+                    result = value1 - value2;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CalculatorApp/CalculatorApp/MainPage.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System.Diagnostics;
+using System.Data;
+
+namespace CalculatorApp
+{
+    public partial class MainPage : ContentPage
+    {
+        string currentInput = string.Empty;
+        string equation = string.Empty;
+        float value;
+        public MainPage()
+        {
+            InitializeComponent();
+            Output.Text = string.Empty;
+            Equation.Text = string.Empty;
+        }
+
+        //Works
+        private void Clear_Clicked(object sender, EventArgs e)
+        {
+            Output.Text = string.Empty;
+            Equation.Text = string.Empty;
+            currentInput = string.Empty;
+            equation = string.Empty;
+        }
+
+        //Error
+        private void Fraction_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(currentInput))
+            {
+                value = 1 / float.Parse(currentInput);
+                currentInput = Convert.ToString(value);
+                equation += currentInput;
+                Equation.Text = currentInput;
+                Output.Text = currentInput;
+            }
+        }
+
+        //Error
+        private void Percent_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(currentInput))
+            {
+                value = float.Parse(currentInput) / 100;
+                currentInput = Convert.ToString(value);
+                equation += currentInput;
+
+                Equation.Text = currentInput;
+                Output.Text = currentInput;
+            }
+        }
+
+        //Error
+        private void Negative_Clicked(object sender, EventArgs e) { 
+            value = float.Parse(currentInput);
+
+            if (!currentInput.Contains("-") && !string.IsNullOrEmpty(equation))
+            {
+                value -= value * 2;
+                currentInput = Convert.ToString(value);
+            }
+            else {
+                currentInput = Convert.ToString(Math.Abs(value));
+            }
+            equation += currentInput;
+            Equation.Text = currentInput;
+            Output.Text = currentInput;
+        }
+
+        //Works
+        private void Decimal_Clicked(object sender, EventArgs e)
+        {
+            if (!currentInput.Contains("."))
+            {
+                currentInput += ".";
+            }
+            equation += currentInput;
+            Equation.Text = currentInput;
+            Output.Text = equation;
+        }
+
+        //Works
+        private void Numbers_Clicked(object sender, EventArgs e)
+        {
+            if (sender is Button button)
+            {
+                currentInput += button.Text;
+                equation = currentInput;
+                Equation.Text = equation;
+                Output.Text = equation;
+            }
+        }
+
+        //Works
+        private void Operators_Clicked(object sender, EventArgs e)
+        {
+            if (sender is Button button)
+            {
+                if (!string.IsNullOrEmpty(equation))
+                {
+                    currentInput += button.Text;
+                    equation = currentInput;
+                    Equation.Text = equation;
+                    Output.Text = equation;
+                }
+            }
+        }
+
+        //Works
+        private void Equals_Clicked(object sender, EventArgs e)
+        {  
+            if (string.IsNullOrEmpty(equation))
+                return;
+
+            equation = Output.Text;
+            var result = new DataTable().Compute(equation, null); ;
+
+            Output.Text = result.ToString();
+            Equation.Text = equation + " = ";
+            currentInput = string.Empty;
+        }
+    }
+}
